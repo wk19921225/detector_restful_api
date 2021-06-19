427d7e474c1a1486f7b20e800db1657a0e78864a
wangkang12 <wangkang12@jd.com>
2021-06-19 21:51:19 +0800

    feat

--- system/image_detector.py
+++ system/image_detector.py
@@ -21,22 +21,55 @@ def decode_base64_str(img_base64_str):
         raise Exception("Do not parse!")
 
 
+MIN_MATCH_COUNT = 10  # 设置最低特征点匹配数量为10
+
+
 def object_detect(target_base64, page_id, url):
     target_image = decode_base64_str(target_base64)
     # 用来匹配的模板图片
-    template = cv2.imdecode(target_image, cv2.IMREAD_UNCHANGED)
-    template_grey = cv2.cvtColor(template, cv2.COLOR_BGR2GRAY)
+    template = cv2.imdecode(target_image, cv2.COLOR_BGR2BGRA)
     # cv2.imwrite(get_save_image_path("template.png"), template_grey)
     file_path = get_image_path(page_id, url)
     # 视觉稿
-    img_rgb = cv2.imread(file_path)
-    img_gray = cv2.cvtColor(img_rgb, cv2.COLOR_BGR2GRAY)
-    cv2.imwrite(get_save_image_path("out.png"), img_gray)
-    channel, w, h = template.shape[::-1]
-    # print(channel, w, h)
-    res = cv2.matchTemplate(img_gray, template_grey, cv2.TM_CCOEFF_NORMED)
-    threshold = 0.4
-    loc = np.where(res >= threshold)
-    for pt in zip(*loc[::-1]):
-        cv2.rectangle(img_rgb, pt, (pt[0] + w, pt[1] + h), (0, 0, 255), 1)
-    cv2.imwrite(get_save_image_path("output.png"), img_rgb)
+    img_relay = cv2.imread(file_path, cv2.COLOR_BGR2BGRA)
+
+    # Initiate SIFT detector创建sift检测器
+    sift = cv2.xfeatures2d.SIFT_create()
+    # find the keypoints and descriptors with SIFT
+    kp1, des1 = sift.detectAndCompute(template, None)
+    kp2, des2 = sift.detectAndCompute(img_relay, None)
+
+    # 创建设置FLANN匹配
+    FLANN_INDEX_KDTREE = 0
+    index_params = dict(algorithm=FLANN_INDEX_KDTREE, trees=5)
+    search_params = dict(checks=50)
+    flann = cv2.FlannBasedMatcher(index_params, search_params)
+    matches = flann.knnMatch(des1, des2, k=2)
+    # store all the good matches as per Lowe's ratio test.
+    good = []
+    # 舍弃大于0.7的匹配
+    for m, n in matches:
+        if m.distance < 0.7 * n.distance:
+            good.append(m)
+    if len(good) > MIN_MATCH_COUNT:
+        # 获取关键点的坐标
+        src_pts = np.float32([kp1[m.queryIdx].pt for m in good]).reshape(-1, 1, 2)
+        dst_pts = np.float32([kp2[m.trainIdx].pt for m in good]).reshape(-1, 1, 2)
+        # 计算变换矩阵和MASK
+        M, mask = cv2.findHomography(src_pts, dst_pts, cv2.RANSAC, 5.0)
+        matchesMask = mask.ravel().tolist()
+        h, w = template.shape
+        # 使用得到的变换矩阵对原图像的四个角进行变换，获得在目标图像上对应的坐标
+        pts = np.float32([[0, 0], [0, h - 1], [w - 1, h - 1], [w - 1, 0]]).reshape(-1, 1, 2)
+        dst = cv2.perspectiveTransform(pts, M)
+        cv2.polylines(img_relay, [np.int32(dst)], True, 0, 2, cv2.LINE_AA)
+    else:
+        print("Not enough matches are found - %d/%d" % (len(good), MIN_MATCH_COUNT))
+        matchesMask = None
+    draw_params = dict(matchColor=(0, 255, 0),
+                       singlePointColor=None,
+                       matchesMask=matchesMask,
+                       flags=2)
+    result = cv2.drawMatches(template, kp1, img_relay, kp2, good, None, **draw_params)
+    plt.imshow(result, 'gray')
+    plt.show()
